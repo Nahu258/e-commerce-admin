643f781969720c9783e7a4f6dd43353f431de1b2
Nahuel <Nahugc258@gmail.com>
2023-09-25 21:35:48 -0300

    Add Orders Page and fetching orders from db

---
+++ pages/orders.js
@@ -0,0 +1,46 @@
+import Layout from '@/components/Layout'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+export default function OrdersPage () {
+  const [orders, setOrders] = useState([])
+  useEffect(() => {
+    axios.get('/api/orders').then(response => {
+      setOrders(response.data)
+    })
+  }, [])
+
+  return (
+    <Layout>
+      <h1>Orders</h1>
+      <table className='basic'>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Recipient</th>
+            <th>Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 && orders.map(order => (
+            <tr key={order._id}>
+              <td>{(new Date(order.createdAt).toLocaleString())}</td>
+              <td>
+                {order.name} - {order.email}<br />
+                {order.country} - {order.postalCode}<br />
+                {order.city} - {order.streetAddress}<br />
+              </td>
+              <td>
+                {order.items.map(item => (
+                  <div key={item._id}>
+                    {item.title} x{item.quantity}<br />
+                  </div>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  )
+}
